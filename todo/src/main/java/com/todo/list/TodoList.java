335813bf6efbe167eaa03f25534bf4e6df9cb33f
chenhonghe <honghe.chen.ch@partner.bmw-brilliance.cn>
2021-01-03 20:03:23 +0800

    去掉了没有用的引用

--- todo/src/main/java/com/todo/list/TodoList.java
+++ todo/src/main/java/com/todo/list/TodoList.java
@@ -3,7 +3,6 @@ package com.todo.list;
 import com.todo.item.Item;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
